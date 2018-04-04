ee981567bb1eed38bd54db161b254f5a71d36677
david-pettifor-nd <dpettifo@nd.edu>
2018-04-04 11:03:51 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='keywordtree',
+      version='0.5.0',
+      description='Data-structure for keyword searches',
+      url='https://dpettifor.crc.nd.edu/software/keyword_tree/',
+      author='David W Pettifor',
+      author_email='dpettifo@nd.edu',
+      license='GPL-3.0',
+      packages=['keywordtree'],
+      install_requires=['pympler'],
+      zip_safe=False)
